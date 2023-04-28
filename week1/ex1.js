ceff1a9d953d8b189e81ffdc7f3debbef73b27f1
yudongyun <yudongyun>
2023-04-28 14:34:30 +0900

    안녕

---
+++ week1/ex1.js
@@ -0,0 +1,12 @@
+
+function fn_over(obj){
+    console.log('콘솔에 프린트됨');
+    console.log(obj);
+    obj.src = 'media/별 이미지.png' ;
+}
+function fn_out(obj){
+    obj.src = 'media/하트 이미지.png' ;
+}
+
+
+
